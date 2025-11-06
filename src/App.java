ed6b60f9a264aa9e70d868249b84fe96bf62de94
CharlieKavich <charleskavich@gmail.com>
2025-11-06 10:49:51 -0800

    Rearrange

--- src/App.java
+++ src/App.java
@@ -1,17 +1,44 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
+
+        ArrayList<Tile> tileList = new ArrayList<Tile>();
+
         
+
+   
         
-        
-        Tile tileA = new Tile('A', 1);
-        Tile tileC = new Tile('C', 3);
-        Tile tileE = new Tile('E', 1);
-        Tile tileH = new Tile('H', 4);
-        Tile tileI = new Tile('I', 1);
-        Tile tileL = new Tile('L', 1);
-        Tile tileR = new Tile('R', 1);
-        
-        
-        Tile.printTile(tileC);
+
+    }
+
+    public static void tileGen(ArrayList<Tile> tileList)
+    {
+        tileList.add(new Tile('A', 1));
+        tileList.add(new Tile('B', 3));
+        tileList.add(new Tile('C', 3));
+        tileList.add(new Tile('D', 2));
+        tileList.add(new Tile('E', 1));
+        tileList.add(new Tile('F', 4));
+        tileList.add(new Tile('G', 2));
+        tileList.add(new Tile('H', 4));
+        tileList.add(new Tile('I', 1));
+        tileList.add(new Tile('J', 8));
+        tileList.add(new Tile('K', 5));
+        tileList.add(new Tile('L', 1));
+        tileList.add(new Tile('M', 3));
+        tileList.add(new Tile('N', 1));
+        tileList.add(new Tile('O', 1));
+        tileList.add(new Tile('P', 3));
+        tileList.add(new Tile('Q', 10));
+        tileList.add(new Tile('R', 1));
+        tileList.add(new Tile('S', 1));
+        tileList.add(new Tile('T', 1));
+        tileList.add(new Tile('U', 1));
+        tileList.add(new Tile('V', 4));
+        tileList.add(new Tile('W', 4));
+        tileList.add(new Tile('X', 8));
+        tileList.add(new Tile('Y', 4));
+        tileList.add(new Tile('Z', 10));
     }
 }
