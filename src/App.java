d205fd0090abb1941de852c3ed1437bf29a517c1
CharlieKavich <charleskavich@gmail.com>
2025-11-10 16:33:34 -0800

    Lab almost done, just needs score method to be completed.

--- src/App.java
+++ src/App.java
@@ -1,15 +1,81 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Iterator<Tile> it;
         System.out.println("Welcome to Scrabble.");
         ArrayList<Tile> tileList = new ArrayList<Tile>();
         tileGen(tileList);
-        Random random = new Random();
-        int ran = random.nextInt(0, 25);
-        System.out.println(ran);
-        System.out.println(tileList.get(ran).toString());
+
+        boolean cont = true;
+        do {
+            ArrayList<Tile> hand = new ArrayList<Tile>();
+            deck(tileList, hand);
+
+            System.out.print("Your tile hand: ");
+            it = hand.iterator();
+            while (it.hasNext()) {
+                System.out.print(it.next().getLetter());
+            }
+
+
+            System.out.print("\nEnter a word: ");
+            String yourWord = in.nextLine();
+            if (check(hand, yourWord))
+            {
+                System.out.println("Yes, you can spell " + yourWord + ". It is worth " + score(tileList, yourWord) + " points.");
+            }
+            else
+            {
+                System.out.println("No, you cannot spell " + yourWord + ".");
+            }
+            cont = false;
+            
+        } while (cont);
+        
+    }
+
+    public static boolean check(ArrayList<Tile> hand, String word)
+    {
+        Iterator<Tile> it;
+        
+        boolean valid = true;
+
+
+        for (int i = 0; i < word.length(); i++) {
+            it = hand.iterator();
+            while (it.hasNext()) {
+                if(it.next().getLetter() == word.toUpperCase().charAt(i))
+                {
+                    valid = true;
+                    break;
+                }
+                else
+                {
+                    valid = false;
+                }
+            }
+        }
+
+        return valid;
+    }
+
+    public static int score(ArrayList<Tile> base,String word)
+    {
+        return 0;
+    }
+
+    public static void deck(ArrayList<Tile> base, ArrayList<Tile> hand)
+    {
+        Random random = new Random(); 
+        for (int i = 0; i < 7; i++) {
+            int ran = random.nextInt(0, 25);
+            hand.add(base.get(ran));
+        }
     }
 
     public static void tileGen(ArrayList<Tile> tileList)
